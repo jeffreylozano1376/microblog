35400e0a83a9efbb42d8bbad4ffe0455fbf43b18
jeffreylozano1376 <jeffreylozano1376@yahoo.com.ph>
2020-08-17 12:23:16 +0800

    fdas

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['julianclark143@gmail.com']
-    LANGUAGES = ['en', 'es']
+    # LANGUAGES = ['en', 'es']
     POSTS_PER_PAGE = 25
